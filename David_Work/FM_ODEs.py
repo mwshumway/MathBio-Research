5f57a33c044e9fd244f4b2ad00800142f765ca52
David Jorgensen <davidj01@byu.edu>
2023-11-13 22:02:39 -0700

    Recent Edits to Equations

--- David_Work/FM_ODEs.py
+++ David_Work/FM_ODEs.py
@@ -5,37 +5,39 @@ from scipy.integrate import odeint
 
 def func(FM,t,a,A_e,A_p,b,f,g,j,k,K_m,S_e,v_m,V,W,y,z):
     P,P_b,P_u,E,E_b,E_u,S = FM
-    dPdt = y - (k*S_e + (k/W)*S)*P + j*P_b + g*(A_e/A_p)*E
+    dPdt = y - (k*S_e + (k/W)*S)*P + j*P_b + f*(A_e/A_p)*E
     dP_bdt = (k*S_e + (k/W)*S)*P - j*P_b - a*P_b
-    dP_udt = a*P_b - f*(A_p/A_e)*P_u
-    dEdt = b*E_u + j*E_b - (k/W)*S*E - g*(A_e/A_p)*E
+    dP_udt = a*P_b - g*(A_p/A_e)*P_u
+    dEdt = b*E_u + j*E_b - (k/W)*S*E - f*(A_e/A_p)*E
     dE_bdt = (k/W)*S*E - j*E_b - a*E_b
-    dE_udt = f*(A_p/A_e)*P_u + a*E_b - b*E_u - z*E_u
+    dE_udt = g*(A_p/A_e)*P_u + a*E_b - b*E_u - z*E_u
     dSdt = -(k/W)*S*((A_p/V)*P+(A_e/V)*E) + (j+a)*((A_p/V)*P_b+(A_e/V)*E_b) - (v_m*S)/(V*(K_m+S))
     dydt = [dPdt, dP_bdt, dP_udt, dEdt, dE_bdt, dE_udt, dSdt]
     return dydt
 
-y0 = [1,0,0,0,0,0,0]
-
 a = 1
-A_e = 47
-A_p = 314
 b = 1
-f = 0.25
-g = 0.1
+f = .1
+g = .1
 j = 10**2
 K_m = 2.5
-k = j/0.74
-v_m = 8.8*10**3
-V = 523
+K_d = .74
+k = j/K_d
+y = .000083
+x = .002
+A_e = 47
+A_p = 314
 W = 32
-y = 0.000083
-z = 0.002
+v_m = 8.8 * 10**3
+V = 523
+z = .002
 
 
-rates = [0.1,0.2,0.3,0.5,.7,1]
+
+y0 = [50,0,0,0,0,0,0]
+rates = [0.01,0.1,0.3,0.5,.7,1,5,10,20]
+t = np.linspace(0,50,1000)
 for i in range(6):
-    t = np.linspace(0,20,10000)
     S_e = rates[i]
     plt1 = plt.subplot(int(f"23{i+1}"))
     sol = odeint(func,y0,t,args = (a,A_e,A_p,b,f,g,j,k,K_m,S_e,v_m,V,W,y,z))
--- David_Work/PM_steady_state.py
+++ David_Work/PM_steady_state.py
@@ -2,21 +2,25 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 constant = 1000
+S_e = np.linspace(0.001,5,constant)
 
-S_e = np.linspace(0.001,1,constant)
-v_m = 8.8 * 10**3
-y = 0.000083
-W = 32
+A_p = 314
 a = 1
 j = 10**2
 K_d = 0.74
-k = j/K_d
 K_m = 2.5
+k = j/K_d
+v_m = 8.8 * 10**3
 V = 523
+W = 32
+y = 0.000083
 z = .002
 
 
-
+P = (-A_p*S_e*W*a*y - A_p*S_e*W*j*y - S_e*W*a*v_m + np.sqrt(S_e*W*(A_p**2*S_e*W*a**2*y**2 + 2*A_p**2*S_e*W*a*j*y**2 + A_p**2*S_e*W*j**2*y**2 + 4*A_p*K_m*a**2*v_m*y + 4*A_p*K_m*a*j*v_m*y - 2*A_p*S_e*W*a**2*v_m*y - 2*A_p*S_e*W*a*j*v_m*y + S_e*W*a**2*v_m**2)))/(2*A_p*S_e*a*k*(K_m - S_e*W))
+P_b = y/a
+P_u = y/z
+S = (S_e*W*(A_p*a*y + A_p*j*y - a*v_m) + np.sqrt(S_e*W*(A_p**2*S_e*W*a**2*y**2 + 2*A_p**2*S_e*W*a*j*y**2 + A_p**2*S_e*W*j**2*y**2 + 4*A_p*K_m*a**2*v_m*y + 4*A_p*K_m*a*j*v_m*y - 2*A_p*S_e*W*a**2*v_m*y - 2*A_p*S_e*W*a*j*v_m*y + S_e*W*a**2*v_m**2)))/(2*a*v_m)
 
 #Steady State
 P = (-S_e*V*W*a*y - S_e*V*W*j*y - S_e*W*a*v_m + np.sqrt(S_e*W*(4*K_m*V*a**2*v_m*y + 4*K_m*V*a*j*v_m*y + S_e*V**2*W*a**2*y**2 + 2*S_e*V**2*W*a*j*y**2 + S_e*V**2*W*j**2*y**2 - 2*S_e*V*W*a**2*v_m*y - 2*S_e*V*W*a*j*v_m*y + S_e*W*a**2*v_m**2)))/(2*S_e*V*a*k*(K_m - S_e*W))
@@ -24,10 +28,12 @@ P_b = y/a
 P_u = y/z
 S = (S_e*W*(V*a*y + V*j*y - a*v_m) + np.sqrt(S_e*W*(4*K_m*V*a**2*v_m*y + 4*K_m*V*a*j*v_m*y + S_e*V**2*W*a**2*y**2 + 2*S_e*V**2*W*a*j*y**2 + S_e*V**2*W*j**2*y**2 - 2*S_e*V*W*a**2*v_m*y - 2*S_e*V*W*a*j*v_m*y + S_e*W*a**2*v_m**2)))/(2*a*v_m)
 
-plt.plot(S_e,P,'b',label = "Plasma Membrane Ground State")
-plt.plot(S_e,[P_b for _ in range(constant)],'g',label = "Plasma Membrane Bound")
-plt.plot(S_e,[P_u for _ in range(constant)],'r',label = "Plasma Membrane Ubiquitinated")
+
+plt.plot(S_e,P,'b',label = "Ground State")
+plt.plot(S_e,[P_b for _ in range(constant)],'g',label = "Bound")
+plt.plot(S_e,[P_u for _ in range(constant)],'r',label = "Ubiquitinated")
 plt.plot(S_e,S,'k',label = "Intracellular Uracil")
 plt.legend()
 plt.xlabel("Extracellular Uracil")
+plt.title("Plasma Membrane Fur4 Steady States")
 plt.show()
