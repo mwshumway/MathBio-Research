66b8f445a0d257ec7c52230a057f01ef3d1f61ef
David Jorgensen <davidj01@byu.edu>
2024-01-16 11:29:29 -0700

    David New

--- David_Work/FM_ODEs.py
+++ David_Work/FM_ODEs.py
@@ -1,8 +1,10 @@
-from sqlite3 import Time
+#Importing Packages
 from matplotlib import pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
 
+
+#Defining ODE 
 def func(FM,t,a,A_e,A_p,b,f,g,j,k,K_m,S_e,v_m,V,W,y,z):
     P,P_b,P_u,E,E_b,E_u,S = FM
     dPdt = y - (k*S_e + (k/W)*S)*P + j*P_b + f*(A_e/A_p)*E
@@ -15,6 +17,9 @@ def func(FM,t,a,A_e,A_p,b,f,g,j,k,K_m,S_e,v_m,V,W,y,z):
     dydt = [dPdt, dP_bdt, dP_udt, dEdt, dE_bdt, dE_udt, dSdt]
     return dydt
 
+#Defining Variables
+A_e = 47
+A_p = 314
 a = 1
 b = 1
 f = .1
@@ -24,9 +29,6 @@ K_m = 2.5
 K_d = .74
 k = j/K_d
 y = .000083
-x = .002
-A_e = 47
-A_p = 314
 W = 32
 v_m = 8.8 * 10**3
 V = 523
@@ -34,24 +36,38 @@ z = .002
 
 
 
-y0 = [50,0,0,0,0,0,0]
-rates = [0.01,0.1,0.3,0.5,.7,1,5,10,20]
-t = np.linspace(0,50,1000)
-for i in range(6):
+#Defining Initial Condition, Timespan, and rates to test
+y0 = [1,0,0,0,0,0,0]
+rates = [0.01,0.2,0.5,0.5,.7,1,5,10,20]
+rates = [.7,1,5,10,20]
+t = np.linspace(0,50000,100000)
+#Going through each rate
+for i in range(3):
+    #Defining Extracellular Uracil
     S_e = rates[i]
+    #Defining subplot
     plt1 = plt.subplot(int(f"23{i+1}"))
+    #Finding solution to ODE
     sol = odeint(func,y0,t,args = (a,A_e,A_p,b,f,g,j,k,K_m,S_e,v_m,V,W,y,z))
+    #Plotting solution
     plt1.plot(t,sol[:,0],'b',label = "PM Ground State")
     plt1.plot(t,sol[:,1],'g',label = "PM Bound")
     plt1.plot(t,sol[:,2],'r',label = "PM Ubiquitinated")
     plt1.plot(t,sol[:,3],'b--',label = "EM Ground State")
     plt1.plot(t,sol[:,4],'g--',label = "EM Bound")
     plt1.plot(t,sol[:,5],'r--',label = "EM Ubiquitinated")
-    plt1.plot(t,sol[:,6],'k',label = "Intracellullar Uracil")
     plt1.set_xlabel("Time")
-    plt1.set_title(f"S_e = {S_e}")
     plt.legend()
     plt.grid()
+    plt1.set_title(f"S_e = {S_e}")
+    plt2 = plt.subplot(int(f"23{i+4}"))
+    plt2.plot(t,sol[:,6],'k',label = "Intracellullar Uracil")
+    plt2.set_xlabel("Time")
+    plt2.set_title("Intracellullar Uracil")
+    #Setting graph layout
+    plt.legend()
+    plt.grid()
+    plt1.set_xlabel("Time")
     print(sol[:,0][-1])
 
 
--- David_Work/PM_steady_state.py
+++ David_Work/PM_steady_state.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 constant = 1000
-S_e = np.linspace(0.001,5,constant)
+S_e = np.linspace(0.001,.05,constant)
 
 A_p = 314
 a = 1
@@ -28,12 +28,33 @@ P_b = y/a
 P_u = y/z
 S = (S_e*W*(V*a*y + V*j*y - a*v_m) + np.sqrt(S_e*W*(4*K_m*V*a**2*v_m*y + 4*K_m*V*a*j*v_m*y + S_e*V**2*W*a**2*y**2 + 2*S_e*V**2*W*a*j*y**2 + S_e*V**2*W*j**2*y**2 - 2*S_e*V*W*a**2*v_m*y - 2*S_e*V*W*a*j*v_m*y + S_e*W*a**2*v_m**2)))/(2*a*v_m)
 
+fig,ax = plt.subplots(1,2)
+ax[0].plot(S_e,P,'b',label = "Ground State")
+ax[0].plot(S_e,[P_b for _ in range(constant)],'g',label = "Bound")
+ax[0].plot(S_e,[P_u for _ in range(constant)],'r',label = "Ubiquitinated")
+ax[0].set_xlabel("Extracellular Uracil")
+ax[0].set_title("Fur4 Steady States")
+ax[0].legend()
 
-plt.plot(S_e,P,'b',label = "Ground State")
-plt.plot(S_e,[P_b for _ in range(constant)],'g',label = "Bound")
-plt.plot(S_e,[P_u for _ in range(constant)],'r',label = "Ubiquitinated")
-plt.plot(S_e,S,'k',label = "Intracellular Uracil")
-plt.legend()
-plt.xlabel("Extracellular Uracil")
-plt.title("Plasma Membrane Fur4 Steady States")
+ax[1].plot(S_e,S,'k')
+ax[1].set_title("Intracellular Uracil")
+ax[1].set_xlabel("Extracellular Uracil")
+
+plt.tight_layout()
+plt.show()
+
+
+
+fig,ax = plt.subplots(1,2)
+
+ax[0].plot(S_e,P+P_b+P_u,'b')
+ax[0].set_xlabel("Extracellular Uracil")
+ax[0].set_ylabel("Total FUR4")
+ax[0].set_title("Total FUR4 vs. Extracellular Uracil")
+ax[1].plot(S_e,S,'r')
+ax[1].set_xlabel("Extracellular Uracil")
+ax[1].set_ylabel("Intracellular Uracil")
+ax[1].set_title("Intracellular Uracil vs. Extracellular Uracil")
+
+plt.tight_layout()
 plt.show()
